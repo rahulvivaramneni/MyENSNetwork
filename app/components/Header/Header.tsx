02d274ceff56493fc9747c0a1a0f9a37d74cbd65
Rahul Vivaramneni <61940373+rahulvivaramneni@users.noreply.github.com>
2023-01-08 02:42:57 +0530

    Update Header.tsx

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,8 +1,8 @@
 export default function Nav2() {
   return (
-    <div className="flex bg-black  items-center justify-center text-white pt-[30px] pb-[30px] [backdrop-filter:blur(4px)]">
-      <p className="font-['Roboto'] inline-flex leading-[30px] text-headertext px-8">
-        MyENSNetwork
+    <div className="flex bg-gray3 rounded-xl	 items-center justify-center text-white pt-[30px] pb-[30px] [backdrop-filter:blur(4px)]">
+      <p className="font-['Roboto'] text-xl	 tracking-wide font-bold inline-flex leading-[30px] text-headertext px-8">
+        My ENS Network
       </p>
     </div>
   );
